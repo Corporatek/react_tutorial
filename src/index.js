ec08c8e2125f84baca3e2923879add37aef69e47
corporatek <kagwunobi@my.uri.edu>
2019-02-24 20:49:55 -0500

    first commit after creating a separate repo for the app

--- src/index.js
+++ src/index.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+import MyInfo from './MyInfo';
 
 import * as serviceWorker from './serviceWorker';
 
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
+// Think of components in terms of where-what-when
 
-function MyApp() {
-    return (
-        <div>
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>    
-    )
-}
+// var element = React.createElement('h1', {className: 'greeting'}, 'Hello, world!');
+
+// function MyApp() {
+//     return (
+//         <div>
+//             <ul>
+//                 <li>1</li>
+//                 <li>2</li>
+//                 <li>3</li>
+//             </ul>
+//         </div>    
+//     )
+// }
 
 
 // Parameters for .render function is (What do I want to load?, where do I want to load it?)
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(element, document.getElementById('root'));
 
-ReactDOM.render(<MyApp />, document.getElementById('root'));
+ReactDOM.render(<MyInfo />, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
