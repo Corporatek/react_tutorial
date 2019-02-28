9cc96ac3f3108ca59f6bd0c4710d13413529ade9
corporatek <kagwunobi@my.uri.edu>
2019-02-27 23:33:35 -0500

    updated todo list using refractored react so that all it needs to do is take in Json parameters.

--- src/components/App.js
+++ src/components/App.js
@@ -3,23 +3,28 @@ import '../App.css';
 // import Footer from "./Footer"
 // import Header from "./Header"
 // import MainContent from "./MainContent"
-// import ToDoItem from "./ToDoItem"
+
+import ToDoItem from "./ToDoItem"
+import ToDoData from "./ToDoData"
+
 // import Joke from "./Joke"
 // import ContactCard from "./ContactCard"
 
-import Product from "./Product";
-import productData from "./vschoolProducts"
+// import Product from "./Product";
+// import productData from "./vschoolProducts"
 
 // Alternatively you can use an arrow function which does not require the return keyword
 
 function App() {
 
-const productList = productData.map(item =>
-<Product key={item.id} name={item.name} price={item.price} description={item.description} />)
+const ToDoList = ToDoData.map(item =>
+<ToDoItem key={item.id} item={item} />)
 
   return (
-    <div className="contacts">
-      {productList}
+    <div className="todo-list">
+    <h1>To Do List...</h1>
+      <hr></hr>
+        {ToDoList}
     </div>
   )
 }
---
+++ src/components/ToDoData.js
@@ -0,0 +1,30 @@
+const ToDoData = [
+    {
+        id: 1,
+        description: "Create deck for Japan Innovation Conference pitch competition",
+        completed: false
+    },
+    {
+        id: 2,
+        description: "File for my return",
+        completed: false
+    },
+    {
+        id: 3,
+        description: "Send resumes and follow up",
+        completed: false
+    },
+    {
+        id: 4,
+        description: "Study React",
+        completed: true
+    },
+    {
+        id: 5,
+        description: "Find time to enjoy myself",
+        completed: false
+    },
+
+]
+
+export default ToDoData
