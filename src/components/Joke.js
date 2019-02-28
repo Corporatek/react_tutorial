f5586c219877f340464aa84ab108464eaf599468
corporatek <kagwunobi@my.uri.edu>
2019-02-27 20:36:23 -0500

    refracted code using best practice for react conditional statements

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -3,21 +3,14 @@ import React from "react"
 function Joke(props) {
     
 // If props of joke has a question return the question and answer.  If props of joke does not have a question only return the answer
-    if (props.joke.question) {
-        return (
-            <div>
-                <p>{props.joke.question}</p>
-                <p>{props.joke.answer}</p>
-            </div>
-        )
-        
-    } else {
-        return(
-            <div>
-                <p>{props.joke.answer}</p>
-            </div>            
-        )
-    }
+    return (
+        <div>
+            {/* The activity under style is another way to create conditional formatting */}
+            <h3 style={{display: !props.joke.question && "none"}}>Question: {props.joke.question}</h3>
+            <h3 style={{color: !props.joke.question && "#888888"}}>Answer: {props.joke.answer}</h3>
+            <hr></hr>
+        </div>
+    )
 }
 
 
